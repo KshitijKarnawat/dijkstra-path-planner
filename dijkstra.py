cba7b352276eb9a7ff1c64b9695c5865da2b1620
KshitijKarnawat <kshitijkarnawat009@gmail.com>
2024-03-05 16:31:34 -0500

    Create Move Functions

--- dijkstra.py
+++ dijkstra.py
@@ -13,3 +13,35 @@ class NewNode:
         self.parent = parent
         self.cost = cost
     
+def move_up(node):
+    x, y = node.coord
+    return NewNode((x, y + 1), node, node.cost + 1)
+
+def move_down(node):
+    x, y = node.coord
+    return NewNode((x, y - 1), node, node.cost + 1)
+
+def move_left(node):
+    x, y = node.coord
+    return NewNode((x - 1, y), node, node.cost + 1)
+
+def move_right(node):
+    x, y = node.coord
+    return NewNode((x + 1, y), node, node.cost + 1)
+
+def move_up_left(node):
+    x, y = node.coord
+    return NewNode((x - 1, y + 1), node, node.cost + 1.4)
+
+def move_up_right(node):
+    x, y = node.coord
+    return NewNode((x + 1, y + 1), node, node.cost + 1.4)
+
+def move_down_left(node):
+    x, y = node.coord
+    return NewNode((x - 1, y - 1), node, node.cost + 1.4)
+
+def move_down_right(node):
+    x, y = node.coord
+    return NewNode((x + 1, y - 1), node, node.cost + 1.4)
+
