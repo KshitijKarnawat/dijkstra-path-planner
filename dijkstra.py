afd73cc0f6b6077784f943c5f7304c18ce3c6c1c
KshitijKarnawat <kshitijkarnawat009@gmail.com>
2024-03-09 15:09:06 -0500

    Implement Dijkstra

--- dijkstra.py
+++ dijkstra.py
@@ -3,12 +3,13 @@ dijkstra.py
 
 @breif:     this module implements Dijkstra's algorithm for finding the shortest path in a graph.
 @author:    Kshitij Karnawat
-@date:      5th March 2024
+@date:      6th March 2024
 @version:   1.0
 """
 
 import numpy as np
 import cv2 as cv
+import time
 
 
 ## OpenCV X axis moves up and down
@@ -22,35 +23,35 @@ class NewNode:
     
 def move_up(node):
     x, y = node.coord
-    return NewNode((x + 1, y), node, node.cost + 1)
+    return NewNode((x + 1, y), node, node.cost + 1), 1
 
 def move_down(node):
     x, y = node.coord
-    return NewNode((x - 1, y), node, node.cost + 1)
+    return NewNode((x - 1, y), node, node.cost + 1), 1
 
 def move_left(node):
     x, y = node.coord
-    return NewNode((x, y - 1), node, node.cost + 1)
+    return NewNode((x, y - 1), node, node.cost + 1), 1
 
 def move_right(node):
     x, y = node.coord
-    return NewNode((x, y + 1), node, node.cost + 1)
+    return NewNode((x, y + 1), node, node.cost + 1), 1
 
 def move_up_left(node):
     x, y = node.coord
-    return NewNode((x + 1, y - 1), node, node.cost + 1.4)
+    return NewNode((x + 1, y - 1), node, node.cost + 1.4), 1.4
 
 def move_up_right(node):
     x, y = node.coord
-    return NewNode((x + 1, y + 1), node, node.cost + 1.4)
+    return NewNode((x + 1, y + 1), node, node.cost + 1.4), 1.4
 
 def move_down_left(node):
     x, y = node.coord
-    return NewNode((x - 1, y - 1), node, node.cost + 1.4)
+    return NewNode((x - 1, y - 1), node, node.cost + 1.4), 1.4
 
 def move_down_right(node):
     x, y = node.coord
-    return NewNode((x - 1, y + 1), node, node.cost + 1.4)
+    return NewNode((x - 1, y + 1), node, node.cost + 1.4), 1.4
 
 def in_obstacles(coord):
     # Set Max and Min values for x and y
@@ -106,58 +107,58 @@ def get_child_nodes(node):
 
     # Create all possible child nodes
     if x < x_max:
-        child = move_up(node)
+        child, child_cost = move_up(node)
         if not in_obstacles(child.coord):
-            child_nodes.append(child)
+            child_nodes.append((child, child_cost))
         else:
             del child
 
     if x > x_min:
-        child = move_down(node)
+        child, child_cost = move_down(node)
         if not in_obstacles(child.coord):
-            child_nodes.append(child)
+            child_nodes.append((child, child_cost))
         else:
             del child
 
     if y < y_max:
-        child = move_right(node)
+        child, child_cost = move_right(node)
         if not in_obstacles(child.coord):
-            child_nodes.append(child)
+            child_nodes.append((child, child_cost))
         else:
             del child
 
     if y > y_min:
-        child = move_left(node)
+        child, child_cost = move_left(node)
         if not in_obstacles(child.coord):
-            child_nodes.append(child)
+            child_nodes.append((child, child_cost))
         else:
             del child
 
     if x < x_max and y < y_max:
-        child = move_up_right(node)
+        child, child_cost = move_up_right(node)
         if not in_obstacles(child.coord):
-            child_nodes.append(child)
+            child_nodes.append((child, child_cost))
         else:
             del child
 
     if x < x_max and y > y_min:
-        child = move_up_left(node)
+        child, child_cost = move_up_left(node)
         if not in_obstacles(child.coord):
-            child_nodes.append(child)
+            child_nodes.append((child, child_cost))
         else:
             del child
 
     if x > x_min and y < y_max:
-        child = move_down_right(node)
+        child, child_cost = move_down_right(node)
         if not in_obstacles(child.coord):
-            child_nodes.append(child)
+            child_nodes.append((child, child_cost))
         else:
             del child
     
     if x > x_min and y > y_min:
-        child = move_down_left(node)
+        child, child_cost = move_down_left(node)
         if not in_obstacles(child.coord):
-            child_nodes.append(child)
+            child_nodes.append((child, child_cost))
         else:
             del child
 
@@ -187,7 +188,7 @@ def create_map():
     hexagon_center = (650, 250)
     hexagon_vertices = []
     for i in range(6):
-        angle_rad = np.deg2rad(60 * i)  # Angle in radians for each vertex
+        angle_rad = np.deg2rad(90) + np.deg2rad(60 * i)  # Angle in radians for each vertex + 90 for rotating the hexagon
         x = int(hexagon_center[0] + side_length * np.cos(angle_rad))
         y = int(hexagon_center[1] - side_length * np.sin(angle_rad))
         hexagon_vertices.append([x, y])
@@ -210,19 +211,87 @@ def create_map():
 
     return game_map
 
-def dijkstra(game_map, start, end):
-    # TODO: Implement Dijkstra's algorithm
-    pass
+def dijkstra(game_map, start, goal):
+    open_list = []
+    open_list_info = {}
+    closed_list = []
+    closed_list_info = {}
+    path = []
 
-def backtrack_path(end_node):
-    # TODO: Implement function to backtrack from end node to start node and return the path
-    pass
+    # Create start node and add it to open list
+    start_node = NewNode(start, None, 0)
+    open_list.append((start_node, start_node.cost))
+    open_list_info[start_node.coord] = start_node
+
+    start_time = time.time()
+    while open_list:
+
+        # Get the node with the minimum cost and add to closed list
+        open_list.sort(key=lambda x: x[1]) # sort open list based on cost
+        node, node_cost = open_list.pop(0)
+        open_list_info.pop(node.coord)
+        closed_list.append(node)
+        closed_list_info[node.coord] = node
+
+        # Check if goal reached
+        if node.coord == goal:
+            end_time = time.time()
+            print("Time taken by Dijkstra: ", end_time - start_time, " seconds")
+            path = backtrack_path(node)
+
+            return path
+        
+        else:
+            children = get_child_nodes(node)
+            for child, child_cost in children:
+                if child.coord in closed_list_info.keys():
+                    del child
+                    continue
+                else:
+                    if child.coord in open_list_info.keys():
+                        if child_cost + node_cost < open_list_info[child.coord].cost:
+                            open_list_info[child.coord].cost = child_cost + node_cost
+                            open_list_info[child.coord].parent = node
+                    else:
+                        child.cost = child_cost + node_cost
+                        child.parent = node
+                        open_list.append((child, child.cost))
+                        open_list_info[child.coord] = child
+
+    end_time = time.time()
+    print("Time taken by Dijkstra: ", end_time - start_time, " seconds")
+    return None
+
+def backtrack_path(goal_node):
+    # TODO: Implement function to backtrack from goal node to start node and return the path
+    path = []
+    parent = goal_node
+    while parent!= None:
+        path.append(parent.coord)
+        parent = parent.parent
+    return path[::-1]
 
 def main():
     game_map = create_map()
     
+    # get start and end points from user
+    start_point = (int(input("Enter x coordinate of start point: ")), int(input("Enter y coordinate of start point: ")))
+    goal_point = (int(input("Enter x coordinate of goal point: ")), int(input("Enter y coordinate of goal point: ")))
+
+    # Check if start and goal points are in obstacles
+    if in_obstacles(start_point):
+        print("Start point is in obstacle")
+        return 
+
+    # find shortest path
+    shortest_path = dijkstra(game_map, start_point, goal_point)
+    if shortest_path == None:
+        print("No path found")
+    else:
+        print("Shortest path: ", shortest_path)
+
     # show map
-    cv.imshow('Map', game_map)
+    # cv.imshow('Map', game_map)
 
     # wait for key press
     cv.waitKey(0)
